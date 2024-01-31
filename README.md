a781e03b7c657e31aa11a7f204defbfd084a7202
Rosane2 <rosanecasas@gmail.com>
2024-01-31 18:30:11 -0300

    Atualizado em 31012024

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Postgresql
 
-Breve Introdução
+# Breve Introdução
    O PostgreSQL é um banco de dados objeto-relacional (sem relação com linguagens de programação orientadas a objetos), em que cada coisa criada é tratada como um objeto, tais como bancos de dados, tabelas, views, triggers, etc.
 
-
+# Boa referência para iniciantes
+ https://www.devmedia.com.br/principais-comandos-sql-com-postgresql/23366
---
+++ criando tabelas/criar-tabela.sql
@@ -0,0 +1,19 @@
+﻿-- Criando o banco de dados
+CREATE DATABASE vendas;
+
+-- Trocar para o banco de dados vendas
+
+-- Criando tabela clientes
+CREATE TABLE clientes (
+    codigo serial PRIMARY KEY,
+    nome varchar(100) NOT NULL,
+    cpf_cnpj varchar(20) NOT NULL,
+    endereco varchar(100),
+    bairro varchar(50),
+    telefone varchar(20),
+    email varchar(100),
+    atualizado timestamp without time zone DEFAULT now(),
+    ativo boolean default true
+)
+
+SELECT * FROM clientes
